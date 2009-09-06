bede899cee7e0007fc2a1ae00849c7a4c55cc526
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-09-06 20:33:36 +0200

    Plugin identification

---
+++ src/module-id.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2009 Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _MODULE_ID_H
+#define _MODULE_ID_H
+
+// Plugin details
+#define PLUGIN_NAME "Buddy Icons Download"
+#define PLUGIN_VERSION  "0.0.3"
+#define PLUGIN_ID_TYPE  "core"
+#define PLUGIN_ID_USER  "potyl"
+#define PLUGIN_ID_NAME  "budicons"
+#define PLUGIN_ID       PLUGIN_ID_TYPE "-" PLUGIN_ID_USER "-" PLUGIN_ID_NAME
+
+#endif
